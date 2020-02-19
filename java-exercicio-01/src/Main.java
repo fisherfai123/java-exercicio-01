3016420b3b4b65ed9a5b20268a85dcb25bda5dfd
Matuk <mateusmatuk@fai-mg.br>
2020-02-19 20:18:11 -0300

    Segundo

--- java-exercicio-01/src/Main.java
+++ java-exercicio-01/src/Main.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
+	public static void main(String[] args) {
+		int menor=0, maior=0, x, cont = 0;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		while(cont < 4) {
+		System.out.println("Insira o valor: ");
+		x = ler.nextInt();
+	    if (cont == 0) {
+		maior = x;
+		menor = x;
+		}
+		
+		if(x > maior) maior = x;
+		if(x < menor) menor = x;
+		
+		cont ++;		
+		}
+		if(maior > 50) System.out.println("Valor muito Alto!");
+		else System.out.println("Valor normal!");
+		
+	}
 }
